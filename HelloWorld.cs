d6fd9565b8a9f1735e26f162bb38df9845db3f67
Avosk <120457217+Avosk@users.noreply.github.com>
2024-11-11 19:00:02 +0300

    Add files via upload

---
+++ HelloWorld.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HelloWorld : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        //сочетание клавиш для комментирования
+        //ctrl+k ctrl+c
+        //вывод текста на печать
+        print("Hello world");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
